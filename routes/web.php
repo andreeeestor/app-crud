ac4ae652e4a74f61bedfdcad97f700ea2b5a99cd
Andre Nestor <andrenestor0503@gmail.com>
2024-04-30 06:57:11 -0300

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\Produto;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/cadastrar', function(Request $informacoes) {
+    Produto::create([
+        "nome" => $informacoes->nome_produto,
+        "descricao" => $informacoes->descricao_produto,
+        "preco" => $informacoes->preco_produto,
+        "quantidade" => $informacoes->quantidade_produto
+    ]);
+    echo "Cadastro realizado com sucesso!";
+    
+});
+
+// Indo buscar uma informação
+Route::get('mostrar-produto/{id}', function($id) {
+
+});
+
